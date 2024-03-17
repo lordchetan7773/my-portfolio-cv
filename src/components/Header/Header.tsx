957889aaa3f7bb8eebb7122ab77014e6706b89c3
chetan sharma <112761766+lordchetan7773@users.noreply.github.com>
2024-03-17 19:28:25 +0530

    header change

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,8 +16,8 @@ export function Header() {
     <Container className="header-fixed">
       <Router>
         <HashLink smooth to="#home" className="logo">
-          <span>{"< Chetan"}</span>
-          <span>{" Sharma / >"}</span>
+          <span>{"<Chetan"}</span>
+          <span>{"Sharma/>"}</span>
         </HashLink>
         <input
           onChange={toggleTheme}
